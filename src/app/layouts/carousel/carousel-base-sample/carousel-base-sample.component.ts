1d7377956a6cd58ab01c516494314559feb65c99
HristoP96 <HPopov@infragistics.com>
2019-12-02 09:33:29 +0200

    add carousel  latest changes

--- src/app/layouts/carousel/carousel-base-sample/carousel-base-sample.component.ts
+++ src/app/layouts/carousel/carousel-base-sample/carousel-base-sample.component.ts
@@ -7,7 +7,7 @@ import { IgxCarouselComponent } from "igniteui-angular";
   styleUrls: ["./carousel-base-sample.component.scss"],
   templateUrl: "./carousel-base-sample.component.html"
 })
-export class CarouselBaseSampleComponent implements OnInit, AfterViewInit {
+export class CarouselBaseSampleComponent implements OnInit {
   @ViewChild("carousel", { static: true }) public carousel: IgxCarouselComponent;
 
   public slides = [
@@ -24,8 +24,6 @@ export class CarouselBaseSampleComponent implements OnInit, AfterViewInit {
 
   public loop = false;
   public current: number;
-  private _prevArrow: HTMLElement;
-  private _nextArrow: HTMLElement;
 
   constructor(private router: Router) { }
 
@@ -33,31 +31,7 @@ export class CarouselBaseSampleComponent implements OnInit, AfterViewInit {
       this.current = this.carousel.current;
       this.carousel.onSlideChanged.subscribe((evt) => {
         this.current = evt.carousel.current + 1;
-        if (this.current === 1) {
-            this._prevArrow.style.opacity = ".5";
-            this._prevArrow.style.pointerEvents = "none";
-            this._nextArrow.style.opacity = "";
-            this._nextArrow.style.pointerEvents = "all";
-
-        } else if (this.current === this.carousel.slides.length) {
-            this._nextArrow.style.opacity = ".5";
-            this._nextArrow.style.pointerEvents = "none";
-            this._prevArrow.style.opacity = "";
-            this._prevArrow.style.pointerEvents = "all";
-
-        } else {
-            this._prevArrow.style.opacity = "";
-            this._nextArrow.style.opacity = "";
-            this._prevArrow.style.pointerEvents = "all";
-            this._nextArrow.style.pointerEvents = "all";
-
-        }
       });
-  }
-
-  public ngAfterViewInit() {
-    this._nextArrow = (document.getElementsByClassName("igx-carousel__arrow--next")[0] as HTMLElement);
-    this._prevArrow = (document.getElementsByClassName("igx-carousel__arrow--prev")[0] as HTMLElement);
   }
 
   public goTo() {
